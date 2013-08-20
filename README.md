ab2d4b24df86bddf1a0ebd77ad0528717b998523
eswai <sakamoto.yuki+github@gmail.com>
2013-08-20 22:41:08 +0200

    major change

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Camerastats is a ruby script to know which camera and lens are popular in a cert
 # Usage
 
 + You must have a Flickr account
++ Install Redis
 + Get api key, shared secret, access token and access secret according to https://github.com/hanklords/flickraw
 + Modify api_key and so on in camerastats.rb
 + Modify group_id of which group you want to analyse in camerastats.rb
@@ -17,34 +18,47 @@ Camerastats is a ruby script to know which camera and lens are popular in a cert
 Something like the following:
 
     Camera ranking
-    111: Nikon D700
-    61: Canon EOS 550D
-    59: Canon EOS 7D
-    56: Nikon D90
-    45: Nikon D3100
-    42: Nikon D5100
-    36: Canon EOS 5D Mark II
-    24: Canon EOS 5D
-    24: Sony SLT-A57
-    20: Canon EOS 50D
-    18: Nikon D3200
-    16: Nikon D70
-    16: Canon EOS 40D
-    16: Nikon D7000
-    15: Sony NEX-7
-    13: Pentax K-5
-    12: Canon EOS 5D Mark III
-    12: Kodak EasyShare Z710 Zoom
+    
+    13:Canon EOS 7D (64)
+    9:Nikon D700 (110)
+    7:Nikon D7000 (16)
+    5:Canon EOS 5D Mark II (14)
+    5:Nikon D5100 (42)
+    4:Canon EOS 550D (62)
+    4:Canon EOS 50D (19)
+    4:Canon EOS 5D Mark III (12)
+    3:Canon EOS 500D (8)
+    3:Nikon D3100 (45)
+    3:Apple iPhone 4S (5)
+    3:Canon EOS 5D (24)
+    2:Nikon D300 (5)
+    2:Sony NEX-7 (15)
+    2:Canon EOS 400D Digital (3)
+    2:Nikon D3200 (18)
+    2:Sony DSLR-A700 (6)
+    2:Nikon D60 (10)
+    2:Canon EOS 60D (5)
+    2:Canon EOS 40D (16)
+    2:Nikon D90 (56)
+    2:Kodak EasyShare Z710 Zoom (12)
+    2:Pentax K-5 (13)
     
     Lens ranking
-    55: 70.0-200.0 mm f/2.8
-    42: 18.0-200.0 mm f/3.5-6.3
-    31: EF70-200mm f/4L IS USM
-    30: EF17-40mm f/4L USM
-    24: 55-200mm f/4-5.6
-    22: 18-55mm f/3.5-5.599999999
-    21: 24.0-70.0 mm f/2.8
-    20: EF100-400mm f/4.5-5.6L IS USM
-    17: 35mm f/1.8
-    12: 70.0-300.0 mm f/4.5-5.6
-    11: 100.0 mm f/2.8
+    
+    7:70.0-200.0 mm f/2.8 (55)
+    4:EF17-40mm f/4L USM (6)
+    4:EF100-400mm f/4.5-5.6L IS USM (20)
+    3:55-200mm f/4-5.6 (24)
+    3:EF24-105mm f/4L IS USM (9)
+    3:24.0-70.0 mm f/2.8 (19)
+    3:18.0-55.0 mm f/3.5-5.6 (9)
+    2:18.0-200.0 mm f/3.5-5.6 (9)
+    2:EF50mm f/1.8 II (2)
+    2:18-200mm f/3.5-5.6 (5)
+    2:EF70-200mm f/2.8L IS USM (4)
+    2:EF70-200mm f/4L IS USM (31)
+    2:EF-S15-85mm f/3.5-5.6 IS USM (2)
+    2:EF-S17-55mm f/2.8 IS USM (9)
+    2:EF-S18-200mm f/3.5-5.6 IS (9)
+    2:18-70mm f/3.5-4.5 (4)
+    2:18-105mm f/3.5-5.6 (2)
--- camerastats.rb
+++ camerastats.rb
@@ -1,4 +1,5 @@
 require 'flickraw'
+require 'redis'
 
 FlickRaw.api_key = "your api key"
 FlickRaw.shared_secret = "your shared secret"
@@ -7,61 +8,117 @@ flickr.access_token = "your access token"
 flickr.access_secret = "your access secret"
 
 group_id = '52694132@N00' # bicycle racing
-pages = 10
+pages = 10 # target photos = pages * 100
 
+redis = Redis.new
+
+# for counting how many photos
 class Counter
   def initialize
     @stats = {}
   end
   
   def add(s)
-    if s and s != ""
-      if @stats.key? s
-        @stats[s] += 1
-      else
-        @stats[s] = 1
-      end
-    end
+    return unless s
+    s.strip!
+    @stats[s] = @stats.key?(s) ? @stats[s] + 1 : 1
+  end
+  
+  def num(s)
+    @stats[s]
   end
   
   def getList
     @stats.to_a.sort{|a, b| b[1] <=> a[1]}
   end
   
-  def to_s
-    getList.map{|x| "#{x[1]}: #{x[0]}"}.join("\n")
+end
+
+# for counting how many users
+class UserCounter
+  def initialize
+    @users = {}
   end
   
+  def add(camera, user)
+    return unless camera
+    camera.strip!
+    if @users.key? camera
+      @users[camera] << user
+      @users[camera].uniq!
+    else
+      @users[camera] = [user]
+    end
+  end
+  
+  def num(camera)
+    @users[camera].size
+  end
+  
+  def getList
+    @users.to_a.sort{|a, b| b[1].size <=> a[1].size}
+  end
 end
 
 n = 1
 camera = Counter.new
 lens = Counter.new
 
+ucamera = UserCounter.new
+ulens = UserCounter.new
+
 1.upto(pages).each do |page|
   photos = flickr.groups.pools.getPhotos :group_id => group_id, :per_page => 100, :page => page 
   
   photos.each do |ph|
-    begin
-      exif = flickr.photos.getExif :photo_id => ph.id
-      c = exif.camera
-      larray = exif.exif.select{|x| x["tag"] == "Lens"}
-      l = larray[0]["raw"] if larray.size > 0
-    
-      camera.add c
-      lens.add l
-      print "#{n}: #{c} + #{l}\n"
-    rescue => ex
-      print "#{n}: #{ex.to_s}\n"
+    if redis.hexists 'exif', ph.id # already cached
+      info = Marshal.load redis.hget('info', ph.id)
+      exif = Marshal.load redis.hget('exif', ph.id)
+      next if exif == 'permission_denied'
+    else # not cached yet
+      begin
+        info = flickr.photos.getInfo :photo_id => ph.id
+        exif = flickr.photos.getExif :photo_id => ph.id
+        redis.hset 'info', ph.id, Marshal.dump(info)
+        redis.hset 'exif', ph.id, Marshal.dump(exif)
+        sleep 0.5
+      rescue => ex
+        print "#{ex.to_s}\n"
+        redis.hset 'info', ph.id, Marshal.dump('permission_denied')
+        redis.hset 'exif', ph.id, Marshal.dump('permission_denied')
+        next
+      end
     end
+    
+    c = exif.camera
+    larray = exif['exif'].select{|x| x["tag"] == "Lens"}
+    l = larray.size > 0 ? larray[0]["raw"] : nil
+    
+    camera.add c
+    lens.add l
+    ucamera.add c, info.owner.nsid
+    ulens.add l, info.owner.nsid
+    
+    puts "#{n}: #{c} + #{l}"
+    
     n += 1
-    sleep 1
   end
+
 end
 
-print "\nCamera ranking\n"
-print camera.to_s
-print "\n\nLens ranking\n"
-print lens.to_s
-print "\n"
+# printing results
+
+puts "", "Camera ranking", ""
+
+ucamera.getList.each do |k, v|
+  next if k == ''
+  puts "#{v.size}:#{k} (#{camera.num(k)})"
+end
+
+puts "", "Lens ranking", ""
+
+ulens.getList.each do |k, v|
+  next if k == ''
+  puts "#{v.size}:#{k} (#{lens.num(k)})"
+end
 
